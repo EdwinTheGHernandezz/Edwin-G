53ed5971d249d97467e99b8ca9f1d7db5000f89d
Edwin Gonzalez <edwincanelogh8@icloud.com>
2022-03-02 15:43:31 -0700

    Last button

--- Edwin G/ViewController.swift
+++ Edwin G/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Pics: UIImageView!
+    
+    @IBOutlet weak var Peace: UIImageView!
+    
+    @IBOutlet weak var Flex: UIImageView!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func colorButtoon(_ sender: Any) {
+        
+        self.view.backgroundColor = UIColor.orange
+        Pics.image = UIImage(named: "IMG_2852")
+        
+        
+    }
     
+    @IBAction func button(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+        Peace.image = UIImage(named:"IMG_2850"
+                              
+                              
+                              
+                              
+    )
+        
+        
+        
+    }
+    
+    @IBAction func buton(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+        Flex.image = UIImage(named: "SNAP_20220220-182357")
 
-
+    }
+    
+    
+    
+    
+    
 }
 
