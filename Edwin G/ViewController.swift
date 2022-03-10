8fd5bcf74905f09bd1317883d2d71b9b58ab68dd
Edwin Gonzalez <edwincanelogh8@icloud.com>
2022-03-10 13:34:48 -0700

    Final Product

--- Edwin G/ViewController.swift
+++ Edwin G/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.orange
         Pics.image = UIImage(named: "IMG_2852")
-        Text1.text = "Orange personality is is more of an adventurous person, also more outgpoing."
+        Text1.text = "Orange personality is is more of an adventurous person, also more outgoing."
         text2.text = "I'm a rebel out of the four tendencies and that just basically someone whos more resistant towards things and is not afraid."
         
     }
@@ -51,13 +51,13 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor.green
         Flex.image = UIImage(named: "SNAP_20220220-182357")
         Text1.text = " And onto the green, it represents thta I can be focused and think to myself and be focused and do what I gotta do "
-        text2.text = "Like for me im in green mode when im working or workinbg out cause im focused and locked in on what I gotta do, and you know relax the mind"
+        text2.text = "Like for me im in green mode when im working or working out cause im focused and locked in on what I gotta do, and you know relax the mind"
     }
     
     @IBAction func Pbutton(_ sender: Any) {
         self.view.backgroundColor = UIColor.purple
         Flex.image = UIImage(named: "IMG_2851")
-        Text1.text = "To me purple is more of a calm just chill personality, I think of it as a verry peaceful person wants no problems ans is just pretty kind"
+        Text1.text = "To me purple is more of a calm just chill personality, I think of it as a verry peaceful person wants no problems and is just pretty kind"
         text2.text = "If i think about it I'm usually in the purple because I'm alwayts chlled out with everyone, dont really like problems but will solve them if have to ;). Basically your typical relaxed dude"
     }
     
